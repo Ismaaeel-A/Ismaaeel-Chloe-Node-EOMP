b1f3ae56020dc3b3090a49665a2fb59cf88f563b
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-08-20 15:49:00 +0200

    access-control

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const port = +process.env.PORT || 4000
 //MIDDLEWARE
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
     next()
 }) 
 
