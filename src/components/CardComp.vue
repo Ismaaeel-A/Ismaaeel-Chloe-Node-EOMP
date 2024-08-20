62e5d9008e89f0c964da49f4b31f6ff2a89bd8c1
Chlowus <chlsmi040904@gmail.com>
2024-08-20 15:57:11 +0200

    cardcomp

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,13 +1,25 @@
-<template lang="">
-    <div>
-        
+<template>
+    <div class="card">
+        <div class="photo">
+            <slot name="prod-img"></slot>
+        </div>
+        <div class="content">
+            <div class="title">
+                <slot name="prod-title"></slot>
+            </div>
+            <div class="bg-title" >
+                <slot name="prod-title"></slot>
+            </div>
+            <div class="feature">
+                <slot name="Desc"></slot>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+name: "CardComp"
 }
 </script>
-<style lang="">
-    
+<style>
 </style>
