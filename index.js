14e47fade20947e2330e80e5e27c4f546b3f7ccf
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-08-21 11:47:44 +0200

    incorrect url error

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import path from 'path'
 import { productRouter } from './controller/ProductController.js'
 import { userRouter, express } from './controller/UserController.js'
+import { errorHandling } from './middleware/ErrorHandling.js'
+import cors from 'cors' 
 
 //CREATE AN EXPRESS APP
 const app = express()
@@ -21,12 +23,24 @@ app.use('/users', userRouter)
 app.use('/products',productRouter)
 app.use( express.static('./static'), express.json(), express.urlencoded({
     extended: true
-}))
+    }),
+    cors()
+)
 
 //ENDPOINT
 app.get('^/$|/EOMP', (req, res) => {
     res.status(200).sendFile(path.resolve('./static/html/index.html'))
 })
+
+app.get('*', (req, res) => {
+    
+    res.json({
+        status: 404,
+        err: 'Resource not found.'
+    })
+})
+
+app.use(errorHandling)
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
